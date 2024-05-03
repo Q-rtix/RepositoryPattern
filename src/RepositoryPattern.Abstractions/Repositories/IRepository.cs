59a00d1af4c034f0fdbe9bb79351435fbbb62a89
Carlos J. Ortiz <ortizdcj@gmail.com>
2024-05-03 14:49:20 -0400

    fix params on get repository methods

--- src/RepositoryPattern.Abstractions/Repositories/IRepository.cs
+++ src/RepositoryPattern.Abstractions/Repositories/IRepository.cs
@@ -28,27 +28,25 @@ public interface IRepository<TEntity> where TEntity : class
 	///     (Optional) <see langword="true" /> to disable change tracking; otherwise, <see langword="false" />. The default is
 	///     <see langword="false" />.
 	/// </param>
+	/// <param name="includes"></param>
 	/// <param name="orderBy">
 	///     (Optional) A lambda expression to specify the order of the retrieved entities.
-	/// </param>
-	/// <param name="includes">
-	///		(Optional) A list with the expressions for related entities to be included with the retrieved entities.
 	/// </param>
 	/// <returns>
 	///     An <see cref="IQueryable{TEntity}" /> with all existing <typeparamref name="TEntity" />
 	///     elements if the <paramref name="filters" /> is null. Otherwise, it only contains elements
 	///     that satisfy the condition specified by <paramref name="filters" />.
 	/// </returns>
-	IQueryable<TEntity> GetMany(Expression<Func<TEntity, bool>>? filters = null,
+	IQueryable<TEntity> GetMany(IEnumerable<Expression<Func<TEntity, object>>>? includes = null,
 		bool disableTracking = false,
 		Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
-		params Expression<Func<TEntity, object>>[]? includes);
+		params Expression<Func<TEntity, bool>>[]? filters);
 
 	/// <summary>
 	///     Asynchronously retrieves a single entity from the repository based on filters criteria.
 	/// </summary>
-	/// <param name="filters">
-	///     A lambda expression to test each entity for a condition.
+	/// <param name="includes">
+	///     (Optional) A list with the expressions for related entities to be included with the retrieved entity.
 	/// </param>
 	/// <param name="disableTracking">
 	///     (Optional) <see langword="true" /> to disable change tracking; otherwise, <see langword="false" />. The default is
@@ -57,40 +55,40 @@ public interface IRepository<TEntity> where TEntity : class
 	/// <param name="cancellationToken">
 	///     (Optional) A <see cref="CancellationToken" /> to observe while waiting for the task to complete.
 	/// </param>
-	/// <param name="includes">
-	///     (Optional) A list with the expressions for related entities to be included with the retrieved entity.
+	/// <param name="filters">
+	///     A lambda expression to test each entity for a condition.
 	/// </param>
 	/// <returns>
 	///     A task that represents the asynchronous operation.
 	///     The task result is the retrieved entity, or null if not found.
 	/// </returns>
 	Task<TEntity?> GetOneAsync(
-		Expression<Func<TEntity, bool>> filters,
-		bool disableTracking = false,
 		IEnumerable<Expression<Func<TEntity, object>>>? includes = null,
-		CancellationToken cancellationToken = default
+		bool disableTracking = false,
+		CancellationToken cancellationToken = default,
+		params Expression<Func<TEntity, bool>>[] filters
 	);
 
-	/// <summary>
-	///     Synchronously retrieves a single entity from the repository based on filters criteria.
-	/// </summary>
-	/// <param name="filters">
-	///     A lambda expression to test each entity for a condition.
-	/// </param>
-	/// <param name="disableTracking">
-	///     (Optional) <see langword="true" /> to disable change tracking; otherwise, <see langword="false" />. The default is
-	///     <see langword="false" />.
-	/// </param>
-	/// <param name="includes">
-	///		(Optional) A list with the expressions for related entities to be included with the retrieved entity.
-	/// </param>
-	/// <returns>
-	///     Retrieved the entity that satisfies the condition specified by <paramref name="filters" /> , or null if not found.
-	/// </returns>
+	///  <summary>
+	///      Synchronously retrieves a single entity from the repository based on filters criteria.
+	///  </summary>
+	///  <param name="includes">
+	///      (Optional) A list with the expressions for related entities to be included with the retrieved entity.
+	///  </param>
+	///  <param name="disableTracking">
+	///      (Optional) <see langword="true" /> to disable change tracking; otherwise, <see langword="false" />. The default is
+	///      <see langword="false" />.
+	///  </param>
+	///  <param name="filters">
+	///      A lambda expressions to test each entity for a condition.
+	///  </param>
+	///  <returns>
+	///      Retrieved the entity that satisfies the condition specified by <paramref name="filters" /> , or null if not found.
+	///  </returns>
 	TEntity? GetOne(
-		Expression<Func<TEntity, bool>> filters,
+		IEnumerable<Expression<Func<TEntity, object>>>? includes = null,
 		bool disableTracking = false,
-		IEnumerable<Expression<Func<TEntity, object>>>? includes = null
+		params Expression<Func<TEntity, bool>>[] filters
 	);
 
 	/// <summary>
@@ -280,7 +278,7 @@ public interface IRepository<TEntity> where TEntity : class
 	/// <exception cref="System.ArgumentException">
 	///     Thrown when no entity matching the filters criteria is found in the repository.
 	/// </exception>
-	Task<TEntity> RemoveOneAsync(Expression<Func<TEntity, bool>> filters, bool saveChanges = false,
+	Task<TEntity> RemoveOneAsync(Expression<Func<TEntity, bool>>[] filters, bool saveChanges = false,
 		CancellationToken cancellationToken = default);
 	
 	/// <summary>
@@ -299,7 +297,7 @@ public interface IRepository<TEntity> where TEntity : class
 	/// <exception cref="System.ArgumentException">
 	///     Thrown when no entity matching the filters criteria is found in the repository.
 	/// </exception>
-	TEntity RemoveOne(Expression<Func<TEntity, bool>> filters, bool saveChanges = false);
+	TEntity RemoveOne(Expression<Func<TEntity, bool>>[] filters, bool saveChanges = false);
 
 	/// <summary>
 	///     Asynchronously removes multiple entities from the repository.
@@ -348,7 +346,7 @@ public interface IRepository<TEntity> where TEntity : class
 	/// <returns>
 	///     A task that represents the asynchronous operation.
 	/// </returns>
-	Task RemoveManyAsync(Expression<Func<TEntity, bool>> filters, bool saveChanges = false,
+	Task RemoveManyAsync(Expression<Func<TEntity, bool>>[] filters, bool saveChanges = false,
 		CancellationToken cancellationToken = default);
 
 	/// <summary>
@@ -361,7 +359,7 @@ public interface IRepository<TEntity> where TEntity : class
 	///     (Optional) <see langword="true" /> to save changes after the entities have been removed; otherwise,
 	///     <see langword="false" />. The default is <see langword="false" />.
 	/// </param>
-	void RemoveMany(Expression<Func<TEntity, bool>> filters, bool saveChanges = false);
+	void RemoveMany(Expression<Func<TEntity, bool>>[] filters, bool saveChanges = false);
 
 	/// <summary>
 	///     Asynchronously saves changes to the repository.
